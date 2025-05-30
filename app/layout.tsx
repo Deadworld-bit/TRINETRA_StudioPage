ea089baa4b611c09d40f87d1978df43d1f299f89
Phan Thanh Duc <67327661+Deadworld-bit@users.noreply.github.com>
2025-05-30 14:56:25 +0700

    Update analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -26,6 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${geistSans.variable} font-sans antialiased bg-amber-50 text-black`}>{children}</body>
+            <Analytics />
         </html>
     );
 }
