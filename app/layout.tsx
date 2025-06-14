d04e4b0d26d324e2acd2c08f3016a680409e7406
Phan Thanh Duc <deadworld128@gmail.com>
2025-06-15 00:16:12 +0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
-import AnimatedGrid from "@/components/AnimatedGrid";
 import "./globals.css";
 
 const geistSans = Geist({
